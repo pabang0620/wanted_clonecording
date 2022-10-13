a4cc74d7aecd5a104f6dbd20eadcbf0000105a08
pabang0620 <115059918+pabang0620@users.noreply.github.com>
2022-10-13 20:10:35 +0900

    Add files via upload

---
+++ slick.js
@@ -0,0 +1,16 @@
+$(function(){
+    $('.big_pha_img_set').slick({
+        slide: 'img', // 슬라이드 되어야 할 태그
+        infinite : true, 	//무한 반복 옵션	 
+        slidesToShow : 1,		// 한 화면에 보여질 컨텐츠 개수
+        slidesToScroll : 1,		//스크롤 한번에 움직일 컨텐츠 개수
+        speed : 100,	 // 다음 버튼 누르고 다음 화면 뜨는데까지 걸리는 시간(ms)
+        arrows : true, 		// 옆으로 이동하는 화살표 표시 여부
+        pauseOnHover : true,		// 슬라이드 이동	시 마우스 호버하면 슬라이더 멈추게 설정
+        vertical : false,		// 세로 방향 슬라이드 옵션
+        prevArrow : "<button type='button' class='slick-prev'>Previous</button>",		// 이전 화살표 모양 설정
+        nextArrow : "<button type='button' class='slick-next'>Next</button>",		// 다음 화살표 모양 설정
+        draggable : true,
+        dotclass :"slick-dots"
+    })
+})
