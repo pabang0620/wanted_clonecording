352ed840c2aa22d9ab6fbabe7805bc25ba19d56c
Wonho Lee <pabang0620@naver.com>
2022-11-18 21:33:24 +0900

    20221118

--- 웹 리액트 과제/react_notice_board/src/componants/Bookmarkcom.jsx
+++ 웹 리액트 과제/react_notice_board/src/componants/Bookmarkcom.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import NavyAll from "./NavyAll";
+import { useDispatch, useSelector } from "react-redux";
 
 const Bookmarkcom = () => {
+  const bookmarkList = useSelector((state) => state.bookmark.bookmarkstate);
   return (
     <div>
       <div>
         {" "}
         <NavyAll />
       </div>
-      <div>{/* 필터? */}</div>
+      <div>북마크</div>
+      <div>{bookmarkList.map()}</div>
     </div>
   );
 };
